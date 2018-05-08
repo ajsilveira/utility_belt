56c6122fd8f52b14347bccd1da82cab73965685d
Ana Jorgelina Silveira <anita_silveira@hotmail.com>
2018-05-07 22:49:55 -0300

    Class lammps_parser constructor implemented

--- utility_belt/tests/test_utility_belt.py
+++ utility_belt/tests/test_utility_belt.py
@@ -3,10 +3,16 @@ Unit and regression test for the utility_belt package.
 """
 
 # Import package, test suite, and other packages as needed
-import utility_belt
+import utility_belt as ub
 import pytest
 import sys
 
 def test_utility_belt_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "utility_belt" in sys.modules
+
+def test_constructor():
+    a=ub.lammps_parser('data/log.lammps')
+    assert a.properties == ['Step', 'Temp','PotEng','KinEng','TotEng','Press','2'] + ['Esoftcor']*16
+    assert a.firstline == 89
+    assert a.lastline == 90
--- utility_belt/utility_belt.py
+++ utility_belt/utility_belt.py
@@ -5,6 +5,28 @@ A set of tools for my work
 Handles the primary functions
 """
 
+class lammps_parser:
+    """
+    A parser for LAMMPS log files.
+
+    Parameters
+    ----------
+    file : string
+        The name of a LAMMPS log file.
+
+    run : int, Optional, default: 0
+        An index denoting which run (counting from 0) will be processed.
+    """
+
+    def __init__(self, file, run=0):
+        count = 0
+        for num, line in enumerate(open(file, 'r').readlines()):
+             if line.startswith('Step') and (count == run):          
+                self.firstline = num + 1
+                self.properties = line.split()
+             if line.startswith('Loop time') and (count == run):
+                 self.lastline = num - 1 
+                 count += 1          
 
 def canvas(with_attribution=True):
     """
@@ -31,4 +53,4 @@ def canvas(with_attribution=True):
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
-    print(canvas())
+    pass
